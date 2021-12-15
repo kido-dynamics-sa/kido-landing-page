03a76207003ef655a8e0750fe39a28315ff0c188
Sara <saracelalfonso@gmail.com>
2021-12-15 14:29:15 -0300

    change map

--- src/sections/international-presence.js
+++ src/sections/international-presence.js
@@ -7,14 +7,30 @@ import Image from "next/image";
 import SectionHeader from "components/section-header";
 import Map from "../../public/assets/map.png";
 
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+let AnimatedBox = motion.custom(Box);
+
+const animationProps = {
+  initial: { opacity: 0, scale: 0.75 },
+  transition: { ease: "easeOut", duration: 0.75 },
+};
+
 export default function InternationalPresence({ section }) {
+  const { ref, inView } = useInView({
+    threshold: 0.25,
+  });
   return (
-    <section sx={{ variant: "section.feature" }}>
+    <section ref={ref} sx={{ variant: "section.feature" }}>
       <Container sx={styles.container}>
         <SectionHeader slogan={section.slogan} title={section.title} />
-        <Box>
+        <AnimatedBox
+          {...animationProps}
+          animate={inView ? { opacity: 1, scale: 1 } : ""}
+        >
           <Image src={Map} alt="worlwilde presence map" />
-        </Box>
+        </AnimatedBox>
       </Container>
     </section>
   );
