843759dbd1fbf5aa85b7a32ee774ab8a0fd68308
Sara <saracelalfonso@gmail.com>
2022-02-24 12:05:41 +0100

    another fix

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -29,7 +29,7 @@ export default function MobileDrawer() {
       width="320px"
       drawerHandler={
         <Box sx={styles.handler}>
-          <IoMdMenu size="26px" />
+          <IoMdMenu size="26px" color="white" />
         </Box>
       }
       open={isDrawerOpen}
